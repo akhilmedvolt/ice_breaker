537eaee3398d10e19081902fed6dc25ae2c8d624
akhilmedvolt <reekithak@gmail.com>
2024-07-01 04:27:34 +0530

    feat: update linkedin scraper with GenAI

--- ice_breaker.py
+++ ice_breaker.py
@@ -3,23 +3,37 @@ from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
 from third_parties.linkedin import scrape_linkedin_profile
 from langchain.chains import LLMChain
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from output_parsers import summary_parser
+from output_parsers import Summary
+from typing import Tuple
 
 load_dotenv()
 
-if __name__ == "__main__":
-    summary_template = """
-    geiven the information {information} about a person from i want you to create:
-    1. a short summary
-    2. two interesting facts about them
-    """
 
+def ice_break_with(name: str) -> Tuple[Summary, str]:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
+
+    summary_template = """
+        given the information {information} about a person from i want you to create:
+        1. a short summary
+        2. two interesting facts about them
+        \n{format_instruction}
+        """
     summary_prompt_template = PromptTemplate(
-        input_value=["information"], template=summary_template
+        input_variables=["information"],
+        template=summary_template,
+        partial_variables={
+            "format_instruction": summary_parser.get_format_instructions()
+        },
     )
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    linkedin_data = scrape_linkedin_profile()
-    chain = summary_prompt_template | llm
-    # chain = LLMChain(llm=llm, prompt=summary_prompt_template)
-    res = chain.invoke(input={"information": linkedin_data})
+    chain = summary_prompt_template | llm | summary_parser
+    res: Summary = chain.invoke(input={"information": linkedin_username})
+    return res, linkedin_data.get("profile_pic_url")
 
-    print(res)
+
+if __name__ == "__main__":
+    print("Ice Breaker Enter")
+    res, linkedin_profile_url = ice_break_with("Akhil Sanker")
